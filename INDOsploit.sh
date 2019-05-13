b1bdc996ef5081b927f4c40ef5de21e78d094b04
INDOnimous <40731452+INDOnimous@users.noreply.github.com>
2019-05-14 05:21:48 +0700

    Add files via upload

---
+++ INDOsploit.sh
@@ -0,0 +1,12 @@
+#/!/use/etc/Bash
+figlet INDOsploit.sh | lolcat 
+ppkg install unstable-repo 
+pkg install metasploit
+dpkg --configure -a
+echo "Ketik msfconsole Untuk menjalankannya ✓✓"
+sleep 2
+echo "Jangan lupa SUBSCRIBE Chanel YouTube kami"
+sleep 2
+echo "OFFICIAL INDOnimous"
+sleep 2
+msfconsole
